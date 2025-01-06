ba0b603e8b51265f69d46939ec6310665bc63726
Untouchable2k <dottage@gmail.com>
2025-01-06 14:16:41 -0600

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,48 +1,57 @@
 
 
+
+
     
     function performSwap_Fixed (
         address _liquidationPair,
-        int Txfee
+        int Txfee,
+        bool isEstimator
     ) external returns (bool success){   
             // Transfer tokens from sender to contract
 
-        address srcToken = ILiquidationPair(_liquidationPair).tokenIn();
-        address dstToken = ILiquidationPair(_liquidationPair).tokenOut();
+                // Create a struct instance to store our main swap data
+        SwapData memory swapData = SwapData({
+            sourceVault: address(ILiquidationPair(_liquidationPair).source()),
+            srcToken: ILiquidationPair(_liquidationPair).tokenIn(),
+            dstToken: ILiquidationPair(_liquidationPair).tokenOut(),
+            amountOut: ITpdaLiquidationPair(_liquidationPair).maxAmountOut(),
+            amountInMax: 0,
+            wethBalance: 0
+        });
+        
 
+            swapData.amountInMax = ITpdaLiquidationPair(_liquidationPair).computeExactAmountIn(swapData.amountOut);
 
-            uint _amountOut2 = ITpdaLiquidationPair(_liquidationPair).maxAmountOut();
-            uint _amountInMax2 = ITpdaLiquidationPair(_liquidationPair).computeExactAmountIn(_amountOut2);
-
-        uint256 wethBalance = IERC20(srcToken).balanceOf(address(this));
-        require(_amountInMax2 < wethBalance, string(abi.encodePacked(
+        uint256 wethBalance = IERC20(swapData.srcToken).balanceOf(address(this));
+        require(swapData.amountInMax < wethBalance, string(abi.encodePacked(
             "Requires more WETH in the contract. Needed: ", 
-            Strings.toString(_amountInMax2), 
+            Strings.toString(swapData.amountInMax), 
             ", Available: ", 
             Strings.toString(wethBalance)
         )));
 
 
 
-        IERC20(srcToken).approve(address(fixedRouter_1), _amountInMax2);
+        IERC20(swapData.srcToken).approve(address(fixedRouter_1), swapData.amountInMax);
             // Perform the swap
             // Perform the swap
-        fixedRouter_1.swapExactAmountOut( ITpdaLiquidationPair(_liquidationPair), address(this), _amountOut2, _amountInMax2, block.timestamp+10);
+        fixedRouter_1.swapExactAmountOut( ITpdaLiquidationPair(_liquidationPair), address(this), swapData.amountOut, swapData.amountInMax, block.timestamp+10);
 
-        address newsource=address(dstToken);
+        address newsource=address(swapData.dstToken);
     
         // Approve the router to spend the source token
-        IERC20(newsource).approve(address(swapRouter), _amountOut2);
+        IERC20(newsource).approve(address(swapRouter), swapData.amountOut);
 
         // Encode data
         
             IV3SwapRouter.ExactInputSingleParams memory params =
                 IV3SwapRouter.ExactInputSingleParams({
-                    tokenIn: newsource,
-                    tokenOut: srcToken,
+                    tokenIn: swapData.dstToken,
+                    tokenOut: swapData.srcToken,
                     fee: 500,
                     recipient: address(this),
-                    amountIn: _amountOut2,
+                    amountIn: swapData.amountOut,
                     amountOutMinimum: 0,
                     sqrtPriceLimitX96: 0
                 });
@@ -55,22 +64,23 @@
         /// @return amountOut The amount of the received token
             // The call to `exactInputSingle` executes the swap.
         uint amountOut = swapRouter.exactInputSingle(params);
-    if(int(amountOut) - int(_amountInMax2) -int(Txfee)<0){
+    if(int(amountOut) - int(swapData.amountInMax) -int(Txfee)<0){
 
 
                     require( int(amountOut)>Txfee,"Txfee cant be greater than AmountOut");
-                    doWETHError_Func_Simple(amountOut, Txfee, _amountInMax2);
+                    doWETHError_Func_Simple(amountOut, Txfee, swapData.amountInMax);
 
         
 
     }
 
-    require(amountOut  >= _amountInMax2+uint(Txfee),"Transaction not profitable yet2");
+    require(amountOut  >= swapData.amountInMax+uint(Txfee),"Transaction not profitable yet2");
 
-    
-        IERC20(srcToken).transfer(msg.sender, amountOut-_amountInMax2);
+        if(isEstimator){
+                    doWETHSuccess_Func(amountOut, swapData.amountInMax);
+        }
+        IERC20(swapData.srcToken).transfer(msg.sender, amountOut-swapData.amountInMax);
 
         return true;
         // emit SwapPerformed(srcToken, dstToken, amountIn, returnAmount);
     }
-
