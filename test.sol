453404da5450dbb4b30b120b5a38b59d6f592a2d
Untouchable2k <dottage@gmail.com>
2025-01-06 14:21:03 -0600

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,57 +1,79 @@
 
-
-
-
-    
-    function performSwap_Fixed (
-        address _liquidationPair,
-        int Txfee,
-        bool isEstimator
-    ) external returns (bool success){   
-            // Transfer tokens from sender to contract
-
-                // Create a struct instance to store our main swap data
-        SwapData memory swapData = SwapData({
-            sourceVault: address(ILiquidationPair(_liquidationPair).source()),
-            srcToken: ILiquidationPair(_liquidationPair).tokenIn(),
-            dstToken: ILiquidationPair(_liquidationPair).tokenOut(),
-            amountOut: ITpdaLiquidationPair(_liquidationPair).maxAmountOut(),
-            amountInMax: 0,
-            wethBalance: 0
-        });
         
+    function performSwap (
+        address _liquidationPair,
+        int Txfee
+    ) external returns (bool success){   
+        uint aucPeriod = ITpdaLiquidationPair(_liquidationPair).targetAuctionPeriod();
+        uint64 aucAt = ITpdaLiquidationPair(_liquidationPair).lastAuctionAt();
+        uint192 lastAucPrice=ITpdaLiquidationPair(_liquidationPair).lastAuctionPrice();
+            // Transfer tokens from sender to contract
+        address sourceVault= address(ILiquidationPair(_liquidationPair).source());
 
-            swapData.amountInMax = ITpdaLiquidationPair(_liquidationPair).computeExactAmountIn(swapData.amountOut);
+        address srcToken = ILiquidationPair(_liquidationPair).tokenIn();
+        address dstToken = ILiquidationPair(_liquidationPair).tokenOut();
 
-        uint256 wethBalance = IERC20(swapData.srcToken).balanceOf(address(this));
-        require(swapData.amountInMax < wethBalance, string(abi.encodePacked(
+
+            uint _amountOut2 = ITpdaLiquidationPair(_liquidationPair).maxAmountOut();
+            uint _amountInMax2 = ITpdaLiquidationPair(_liquidationPair).computeExactAmountIn(_amountOut2);
+
+        uint256 wethBalance = IERC20(srcToken).balanceOf(address(this));
+        require(_amountInMax2 < wethBalance, string(abi.encodePacked(
             "Requires more WETH in the contract. Needed: ", 
-            Strings.toString(swapData.amountInMax), 
+            Strings.toString(_amountInMax2), 
             ", Available: ", 
             Strings.toString(wethBalance)
         )));
 
 
 
-        IERC20(swapData.srcToken).approve(address(fixedRouter_1), swapData.amountInMax);
+        IERC20(srcToken).approve(address(router), _amountInMax2);
             // Perform the swap
             // Perform the swap
-        fixedRouter_1.swapExactAmountOut( ITpdaLiquidationPair(_liquidationPair), address(this), swapData.amountOut, swapData.amountInMax, block.timestamp+10);
+        router.swapExactAmountOut(ITpdaLiquidationPair(_liquidationPair),
+                address(this), _amountOut2, _amountInMax2, block.timestamp+10
+            );
+        uint amountRealOut = _amountOut2;
+        address newsource=address(dstToken);
+        if(sourceVault ==newsource){
+            IERC20(dstToken).approve(sourceVault, _amountOut2);
+            // Redeem the exact amount out
+            amountRealOut = IVault(sourceVault).redeem(_amountOut2, address(this), address(this));
 
-        address newsource=address(swapData.dstToken);
-    
+            newsource = IVault(sourceVault).asset();
+        }else if(sourceVault==address(0x9d7A789ED31e501303B3856A58bDD9B41b7a6077)){
+                IERC20(dstToken).approve(address(AavePool), _amountOut2);
+
+            uint out =  AavePool.withdraw(address(srcToken), type(uint256).max, address(this));
+        // Call the WETH deposit function and send ETH
+                newsource=srcToken;
+                amountRealOut = out;
+        }
+        
+        if(newsource ==_WETH_Address ){
+            if(int(amountRealOut) - int(_amountInMax2)- int(Txfee) < 0){
+
+                
+                    require( int(amountRealOut)>Txfee,"Txfee cant be greater than AmountOut");
+                    uint test = uint(int(amountRealOut)-Txfee);
+                    doWETHError_Func(aucAt, aucPeriod, lastAucPrice, test);
+
+            }
+            IERC20(newsource).transfer(msg.sender, amountRealOut-_amountInMax2);
+            return true;
+        }
         // Approve the router to spend the source token
-        IERC20(newsource).approve(address(swapRouter), swapData.amountOut);
+        IERC20(newsource).approve(address(swapRouter), _amountOut2);
 
         // Encode data
         
             IV3SwapRouter.ExactInputSingleParams memory params =
                 IV3SwapRouter.ExactInputSingleParams({
-                    tokenIn: swapData.dstToken,
-                    tokenOut: swapData.srcToken,
+                    tokenIn: newsource,
+                    tokenOut: srcToken,
                     fee: 500,
                     recipient: address(this),
-                    amountIn: swapData.amountOut,
+                    amountIn: _amountOut2,
                     amountOutMinimum: 0,
                     sqrtPriceLimitX96: 0
                 });
@@ -64,23 +86,24 @@
         /// @return amountOut The amount of the received token
             // The call to `exactInputSingle` executes the swap.
         uint amountOut = swapRouter.exactInputSingle(params);
-    if(int(amountOut) - int(swapData.amountInMax) -int(Txfee)<0){
+    if(int(amountOut) - int(_amountInMax2) -int(Txfee)<0){
 
 
                     require( int(amountOut)>Txfee,"Txfee cant be greater than AmountOut");
-                    doWETHError_Func_Simple(amountOut, Txfee, swapData.amountInMax);
+                    uint test = uint(int(amountOut)-Txfee);
+                    doWETHError_Func(aucAt, aucPeriod, lastAucPrice, test);
 
         
 
     }
 
-    require(amountOut  >= swapData.amountInMax+uint(Txfee),"Transaction not profitable yet2");
+    require(int(amountOut)  >= int(_amountInMax2)+Txfee,"Transaction not profitable yet2");
 
-        if(isEstimator){
-                    doWETHSuccess_Func(amountOut, swapData.amountInMax);
-        }
-        IERC20(swapData.srcToken).transfer(msg.sender, amountOut-swapData.amountInMax);
+    
+        IERC20(srcToken).transfer(msg.sender, amountOut-_amountInMax2);
 
         return true;
         // emit SwapPerformed(srcToken, dstToken, amountIn, returnAmount);
     }
+
+
